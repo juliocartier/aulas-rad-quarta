ce9fe32ad2aaa49cd440ffe853b91b23584acc33
Julio Cartier <juliocartier@gmail.com>
2025-06-04 19:53:20 -0300

    Adicionando o Dockerfile

--- projeto_tkinter/main.py
+++ projeto_tkinter/main.py
@@ -23,8 +23,8 @@ class UsuarioApp:
         
         tk.Button(btn_frame, text="Listar Usuarios", command=self.listar_usuarios).grid(row=0, column=0, padx=5)
         tk.Button(btn_frame, text="Criar Usuario", command=self.criar_usuarios).grid(row=0, column=1, padx=5)
-        tk.Button(btn_frame, text="Atualizar Email").grid(row=0, column=2, padx=5)
-        tk.Button(btn_frame, text="Deletar Usuario").grid(row=0, column=3, padx=5)
+        tk.Button(btn_frame, text="Atualizar Email", command=self.atualizar_usuarios).grid(row=0, column=2, padx=5)
+        tk.Button(btn_frame, text="Deletar Usuario", command=self.deletar_usuarios).grid(row=0, column=3, padx=5)
         
         self.email_var = tk.StringVar()
         self.senha_var = tk.StringVar()
@@ -69,6 +69,43 @@ class UsuarioApp:
                 messagebox.showerror("Erro", response.json())
         except Exception as e:
             messagebox.showerror("Erro", f"Erro na requisição ao criar o usuario {e}")
+            
+    def deletar_usuarios(self):
+        selected_item = self.tree.selection()
+        if not selected_item:
+            messagebox.showwarning("Aviso", "Selecione um usuario antes de deletar")
+            return
+        
+        user_id =self.tree.item(selected_item[0])["values"][0]
+        try:
+            response = requests.delete(f"{API_URL}/deletar_usuarios/{user_id}")
+            if response.status_code == 200:
+                messagebox.showinfo("Sucesso", "Usuario deletado")
+            else:
+                messagebox.showerror("Erro", "Erro ao deletaro usuario")
+        except Exception as e:
+            messagebox.showerror("Erro", f"Erro na requisição de deletar o usuario: {e}")
+            
+    def atualizar_usuarios(self):
+        selected_item = self.tree.selection()
+        novo_email = self.novo_email_var.get()
+        if not selected_item or not novo_email:
+            messagebox.showwarning("Aviso", "Selecione um usuario e como também o campo de email para ser digitado.")
+            return
+        
+        user_id = self.tree.item(selected_item[0])["values"][0]
+        payload = {"email": novo_email}
+        
+        try:
+            response = requests.put(f"{API_URL}/atualizar_usuarios/{user_id}", json=payload)
+            if response.status_code == 200:
+                messagebox.showinfo("Sucesso", "Email atualizado")
+            else:
+                messagebox.showerror("Erro", "Erro ao atualizar o usuario")
+        except Exception as e:
+            messagebox.showerror("Erro", f"Erro na requisição {e}")
+            
+            
         
 if __name__ == "__main__":
     root = tk.Tk()
